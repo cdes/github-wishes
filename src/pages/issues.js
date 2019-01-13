5f1e3cdd9f684b7e8f73c2cd5cb0a1a0e60eec0b
Omar Bahareth <omar@omar.engineer>
2019-01-13 22:09:54 +0300

    Add WIP issues page
    Still a WIP. Using this to test if auth is successful.

---
+++ src/pages/issues.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import client from '../clients/github'
+
+class IssuesPage extends React.Component {
+  fetchIssues(owner = 'cdes', repo = 'github-wishes') {
+    client
+      .query({
+        query: gql`{
+        search(type: ISSUE, query: "repo: ${owner}/${repo} label:wishes is:issue is:open sort:reactions-+1-desc", first: 100) {
+          edges {
+            node {
+              ... on Issue {
+                id
+                title
+                ups: reactions(content: THUMBS_UP) {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
+      }`,
+      })
+      .then(result => {
+        console.log(result)
+        this.setState({ issues: result })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchIssues()
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Issues" />
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
+
+export default IssuesPage
